adee03ef5fc3b6629180343f69c3486ebb1ec653
qh1059577797 <50884490+qh1059577797@users.noreply.github.com>
2020-09-10 16:41:00 +0800

    Add files via upload

---
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import com.kuang.pojo.User;
+import com.kuang.pojo.UserT;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        //Spring容器,就类似婚介网站!
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserT user = (UserT) context.getBean("u3");
+        user.show();
+    }
+}
